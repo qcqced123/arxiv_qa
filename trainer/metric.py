1861efa725fb50b40511002f7bc96f2a0d76093a
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2024-09-01 20:08:18 +0900

    Update metric.py

--- trainer/metric.py
+++ trainer/metric.py
@@ -186,10 +186,11 @@ def pair_cosine_sim(a: Tensor, b: Tensor, eps=1e-8) -> Tuple[float, float]:
     b_norm = b / torch.clamp(b_n, min=eps)
     sim_mt = torch.mm(a_norm, b_norm.transpose(0, 1))
 
+    batch = a.shape[0]
     pos_sum = torch.trace(sim_mt)
 
-    pos_score = pos_sum.mean().item()
-    neg_score = (torch.sum(sim_mt) - pos_sum).mean().item()
+    pos_score = (pos_sum / batch).item()
+    neg_score = ((torch.sum(sim_mt) - pos_sum) / batch).item()
     return pos_score, neg_score
 
 
